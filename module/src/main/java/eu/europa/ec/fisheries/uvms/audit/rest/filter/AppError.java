aa96c949498dd0d9c5944cf10cb9830fb3d0a4d8
Emma Fredriksson <emfr87@gmail.com>
2021-09-17 15:15:58 +0200

    Fix code, change class variable to non-public

--- module/src/main/java/eu/europa/ec/fisheries/uvms/audit/rest/filter/AppError.java
+++ module/src/main/java/eu/europa/ec/fisheries/uvms/audit/rest/filter/AppError.java
@@ -2,18 +2,24 @@ package eu.europa.ec.fisheries.uvms.audit.rest.filter;
 
 public class AppError {
 
-    public Integer code;
-    public String  description;
+    private Integer code;
+    private String  description;
 
     public AppError() {
         this.code = Integer.MIN_VALUE;
         this.description = "";
-
     }
-
 
     public AppError(Integer code, String description) {
         this.code = code;
         this.description = description;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public String getDescription() {
+        return description;
     }
 }
